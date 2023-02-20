4688ddfe30ad6f8b7f33f50071c1cabd121465c6
kai <hank1313_net@yahoo.com.tw>
2023-02-21 04:35:16 +0800

    新增token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ContextDashbard from './ContextDashbard'
+import Form from './pages/Form/Form'
 import Map from './pages/Map/Map'
 
 const App = () => {
   return (
     <ContextDashbard>
-      <Map />
+      {/* <Map /> */}
+      <Form />
     </ContextDashbard>
   )
 }
---
+++ src/pages/Form/Form.js
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react'
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const submit = async (data) => {
+    try {
+      const r = await axios.post('http://localhost:3005/login', data)
+      console.log(r.data)
+      if (r.data.success) {
+        const { account, accountId, token } = r.data
+        localStorage.setItem(
+          'myAuth',
+          JSON.stringify({
+            account,
+            accountId,
+            token,
+          })
+        )
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit(submit)}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            {...register('account')}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            {...register('password')}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Form
