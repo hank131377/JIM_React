50a2ac1aa34478d30b11a4325c58b13656b7e8b8
kai <hank1313_net@yahoo.com.tw>
2023-02-22 06:39:57 +0800

    加入LinePay

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import ContextDashbard from './ContextDashbard'
 import Form from './pages/Form/Form'
+import LinaPay from './pages/LinePay/LinePay'
 import Map from './pages/Map/Map'
 
 const App = () => {
   return (
     <ContextDashbard>
-      <Map />
-      {/* <Form /> */}
+      <Routes>
+        <Route path="/" element={<Map />}></Route>
+        <Route path="/linePay/comfirm" element={<LinaPay />}></Route>
+        {/* <Form /> */}
+      </Routes>
     </ContextDashbard>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -10,7 +11,9 @@ import 'bootstrap/dist/js/bootstrap.bundle'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
 
